b90c371ba044900e07e431c4ecb12a07ed621296
zsimo <zsimo30@gmail.com>
2014-04-25 15:24:02 +0200

    added quotes

--- php/files.php
+++ php/files.php
@@ -6,27 +6,69 @@
  */
 // set file to read
 $file = '/usr/local/stuff/that/should/be/elsewhere/recipes/omelette.txt' or die('Could not open file!');
+
 // open file
 $fh = fopen($file, 'r') or die('Could not open file!');
-// read file contents
+// read file contents (returns a string)
 $data = fread($fh, filesize($file)) or die('Could not read file!');
 // close file
 fclose($fh);
-// print file contents**
-echo $data;
+
+/*
+ * OPPURE file
+ */
+// read the file directly into array (each line is an array entries)
+$data = file($file) or die('Could not read file!');
+// foreach ($data as $line) {
+	// echo $line;
+// }
+
+
+/*
+ * OPPURE file_get_contents
+ */
+// read the file directly into a string
+$data = file_get_contents($file) or die('Could not read file!');
+
+
+// print file contents
+print_r($data);
+
+
+/*
+ * OPEN and WRITE a file
+ */
+// set file to write
+$file = '/tmp/dump.txt';
+
+// open file (w: write)(a: append)
+// se il file non esiste, lo crea
+$fh = fopen($file, 'w') or die('Could not open file!');
+
+// write to file
+fwrite($fh, "Look, Ma, I wrote a file!") or die('Could not write to file');
+
+// close file
+fclose($fh);
+
+/*
+ * OPPURE file_put_contents
+ */
+// Notice that double quotes are needed to convert into a carriage return.
+file_put_contents($filename, "Look, Ma, I wrote a file!
+") or die('Could not write to file');
 
 
 
 
 
-
-
-
-
-
-
-
-
+/*
+ * a bunch of functions that allow you to test the status of a file
+ */
+// file_exists($filename)
+// is_dir($filename)
+// is_file($filename)
+// filesize($filename)
 
 
 
---
+++ php/quotes.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * QUOTES
+ */
+
+
+$var = "simone";
+
+// single quotes display chars as is it
+// stampa $var
+print_r('$var');
+
+// double quotes evaluate variable and escapes chars
+// stampa il contenuto della variabile
+print_r("$var"); // oppure
+print_r("{$var}");
+
+
+
+
+
+
+
+
+
+
+
+?>
