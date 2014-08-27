b6f28b97a9f51d2443dfe01e9b5956dc9cbb8fdb
zsimo <zsimo30@gmail.com>
2014-08-27 17:57:13 +0200

    added js folder and delete and array scripts

---
+++ js/delete.js
@@ -0,0 +1,13 @@
+// http://bonsaiden.github.io/JavaScript-Garden/#core.delete
+
+// variabile e funzioni globali non possono essere cancellate
+
+// le propieta di un oggetto si
+var a = {};
+a.uno = "pippo 1";
+a.due = "pippo 2";
+
+console.log(a);
+console.log(delete a.uno); // return true
+console.log(a);
+console.log(a.uno); // undefined
