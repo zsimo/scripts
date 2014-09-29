52b4fba873a43dc0bf9938f5e1d9e9d20b79bac8
zsimo <zsimo30@gmail.com>
2014-09-29 22:57:04 +0200

    aggiunto forEach su array e oggetti

--- js/array.js
+++ js/array.js
@@ -8,17 +8,19 @@ a.lenght = []; // slow
 // forEach (IE9)
 // ciclare su un array
 var arr = [1, 2, 3, 4];
+var obj = {
+	"uno-chiave": "uno-valore",
+	"due-chiave": "due-valore",
+	"tre-chiave": "tre-valore"
+};
+
 var callBack = function (value, index, i_array) {
 	console.log(value);
 	console.log(index);
 	console.log(i_array);
 };
 arr.forEach(callBack);
-
+console.log("*******************");
 // ciclare su un oggetto
-var obj = {
-	"uno-chiave": "uno-valore",
-	"due-chiave": "due-valore",
-	"tre-chiave": "tre-valore"
-};
+
 Object.keys(obj).forEach(callBack);
